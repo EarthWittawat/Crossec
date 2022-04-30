28278e78371d671977062f410e624a362c31c685
earthwittawat2548 <63118195+earthwittawat2548@users.noreply.github.com>
2022-04-30 19:50:28 +0700

    Update streamlit_ui.py

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -21,7 +21,7 @@ if os.path.exists('crossec_model.pkl') == False:
     MODEL_URL = "https://dl.dropboxusercontent.com/s/9ayzkc9jcam5adf/crossec_model.pkl?dl=0"
     urllib.request.urlretrieve(MODEL_URL,"crossec_model.pkl")
 else:
-    learn_inf = load_learner('crossec_model.pkl')
+    learn_inf = load_learner(Path()/'crossec_model.pkl')
 
 tissue = [
  'C3DicotStemSecondary', 'C3DicotStemPrimary'
