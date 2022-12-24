024a85e3ac13d019c6ce580a0dd1f248b86175e6
earthwittawat2548 <earthwittawat2005@gmail.com>
2022-12-24 12:46:39 +0700

    new

---
+++ 1_dashboard.py
@@ -0,0 +1,68 @@
+import time  # to simulate a real time data, time loop
+
+from PIL import Image
+import numpy as np  # np mean, np random
+import pandas as pd  # read csv, df manipulation
+import plotly.express as px  # interactive charts
+import streamlit as st  # 🎈 data web app development
+import os
+
+from csv import writer
+# read csv from a github repo
+st.set_page_config(
+    page_title="Crossec Dashboard",
+    layout="wide",
+)  
+room = st.sidebar.selectbox(
+    "เลือกห้องเรียน",
+    ("ม.5/1", "ม.5/2", "ม.5/3", "ม.5/11")
+)
+with st.sidebar:
+    add_radio = st.radio(
+        "เลือกข้อมูลที่ต้องการแสดง",
+        ("Dashboard", "คลังรูปภาพ")
+    )
+
+image = Image.open('Logo.png')
+with st.container():
+    st.image(image,width=200)
+    st.subheader("Let's make cross section easier!")
+    st.write("")
+    st.subheader("Dashboard")
+    if add_radio == "Dashboard":
+        st.dataframe(pd.read_csv(room+'.csv')) 
+    elif add_radio == "คลังรูปภาพ":
+        tissue = ['ใบพืช C3', 'ใบพืช C4', 'รากพืชใบเลี้ยงคู่ระยะปฐมภูมิ','รากพืชใบเลี้ยงคู่ระยะทุติยภูมิ','ลำต้นพืชใบเลี้ยงคู่ระยะปฐมภูมิ','ลำต้นพืชใบเลี้ยงคู่ระยะทุติยภูมิ','รากพืชใบเลี้ยงเดี่ยวระยะปฐมภูมิ','ลำต้นพืชใบเลี้ยงเดี่ยวระยะปฐมภูมิ']
+        with st.container():
+            path = './predicted/'+ room
+            path = os.path.join(path)
+            cpt = sum([len(files) for r, d, files in os.walk(path)])
+            student = []
+            stu_num = len(os.listdir(path))
+            tissue_available = []
+            for stu in range(stu_num):
+                student.append(os.listdir(path)[stu])
+                for j in range(8):
+                    if os.path.isdir(path + '/' + student[stu] + '/' + tissue[j]):
+                        tissue_available.append(tissue[j])
+            for i in range(len(set(tissue_available))):
+                    st.text("")
+                    st.write(tissue_available[i])
+                    for stu in range(stu_num):
+                        _path = os.path.join(path,student[stu],tissue_available[i])
+                        _cpt = sum([len(files) for r, d, files in os.walk(_path)])
+                        array = [1,1,1]
+                        col = st.columns(array)
+                        k=0
+                        for j in range(_cpt):
+                            if k == 3:
+                                k=0
+                            if k%3 == 0 and k!=0:
+                                col = st.columns(array)
+                            col[k].image(f"{_path}/{j}.jpg",width=400, caption=student[stu])
+                            k+=1
+
+
+
+
+        
---
+++ main.py
@@ -0,0 +1,136 @@
+from this import d
+from PIL import Image
+from fastai.vision.widgets import *
+from fastai.vision.all import *
+from torchvision import models, transforms
+import torch
+import streamlit as st
+import time
+import os
+import os.path
+import urllib.request
+from pathlib import Path
+from csv import writer
+import pathlib
+
+st.set_page_config(
+    page_title="Crossec ML",
+    layout="wide",
+)  
+with st.sidebar:
+    st.image('Logo.png',width=100)
+    st.image('aib.png',width=100)
+    st.header("About this Project✔")
+    st.write("Project นี้ได้รับการสนับสนุนจากโครงการ [AI Builder 2022](https://ai-builders.github.io/) โดยจัดทำขึ้นเพื่อเป็นตัวช่วยในการส่งเสริมการทำปฏิบัติการภาพตัดขวาง โดยทีมผู้พัฒนาได้จัดทำ Dataset และเผยแพร่ [Open Source](https://www.kaggle.com/datasets/earthwttw/plant-tissue-cross-section-dataset) ไว้บนเว็บไซต์ Kaggle",unsafe_allow_html=True)
+    st.header("Developer👨‍💻")
+    st.text("Wittawat Kitipatthavorn\nNawapat Jongaouyporn\nSorratt Treenuson")
+file_exists = os.path.exists('crossec_model.pkl')
+
+plt = platform.system()
+if plt == 'Windows': pathlib.PosixPath = pathlib.WindowsPath
+
+@st.cache(allow_output_mutation=True,show_spinner=False,ttl=1800,max_entries=2,persist=True)
+def load_model(): return load_learner(Path()/'crossec_model.pkl',cpu=True)
+
+tissue = [
+ 'รากพืชใบเลี้ยงคู่ระยะปฐมภูมิ', 'ลำต้นพืชใบเลี้ยงคู่ระยะปฐมภูมิ', 'ลำต้นพืชใบเลี้ยงคู่ระยะทุติยภูมิ','รากพืชใบเลี้ยงเดี่ยวระยะปฐมภมิ','ลำต้นพืชใบเลี้ยงเดี่ยวระยะปฐมภูมิ'
+]
+
+def predict(image, learn):
+        """Return top 5 predictions ranked by highest probability.
+        Parameters
+        ----------
+        :param image: uploaded image
+        :type image: jpg
+        :rtype: list
+        :return: top 5 predictions ranked by highest probability
+        """
+        # create a ResNet model
+        pred, pred_idx, pred_prob = learn.predict(image)
+
+        classes = tissue[int(pred_idx)]
+        
+        return [(classes, pred_prob[pred_idx])]
+
+
+image = Image.open('Logo.png')
+
+with st.container():
+    st.image(image,width=200)
+
+    st.subheader("Let's make cross section easier!")
+    st.write("")
+    file_up = st.file_uploader("Upload an image", type = "jpg")
+    if file_up is not None:
+        image = PILImage.create(file_up)
+        st.image(image, caption = 'Uploaded Image.', use_column_width = True)
+        st.write("")
+        if st.button('ตรวจสอบ'):
+            # display image that user uploaded
+            labels = predict(image,load_model())
+
+            my_bar = st.progress(0)
+
+            for percent_complete in range(100):
+                time.sleep(0.05)
+                my_bar.progress(percent_complete + 1)
+            score = labels[0][1]
+                # print out the top 5 prediction labels with scores
+            st.success(f"Result: {labels[0][0]} {score*100:.02f}%")
+        st.write("")
+        with st.expander("ส่งงาน"):
+            name = st.text_input('ชื่อ-นามสกุล')
+            room = st.selectbox(
+                'ห้อง',
+                ['ม.5/1','ม.5/2','ม.5/3','ม.5/11']
+            )
+            num = st.text_input('เลขที่')
+            tissue = st.selectbox(
+                'เลือกหัวข้อปฏิบัติการ',
+                ['พืชใบเลี้ยงเดี่ยวส่วนลำต้นระยะปฐมภูมิ','พืชใบเลี้ยงเดี่ยวส่วนรากระยะปฐมภูมิ','พืชเลี้ยงคู่ส่วนลำต้นปฐมระยะภูมิ','พืชเลี้ยงคู่ส่วนรากระยะปฐมภูมิ','พืชใบเลี้ยงเดี่ยวส่วนลำต้นระยะทุติยภูมิ','พืชใบเลี้ยงเดี่ยวส่วนรากระยะทุติยภูมิ','พืชเลี้ยงคู่ส่วนลำต้นปฐมระยะทุติยภูมิ','พืชเลี้ยงคู่ส่วนรากระยะทุติยภูมิ','ใบพืช C3', 'ใบพืช C4']
+            )
+            user={'Name':name,'Room':room,'Number':num}
+            if st.button('Send'):
+                if name != "" and num != "":
+                #     noobcopycatch = glob.glob('./predicted/'+ labels[0][0]+'/'+file_up.name)
+                #     if noobcopycatch != []:
+                #         st.write("อย่าโกงงงง")
+                #     else:
+                #         count = 0
+                #         path = './predicted/' + labels[0][0]
+                #         isExist = os.path.exists(path)
+                #         if not isExist:
+                # # Create a new directory because it does not exist 
+                #             os.makedirs(path)
+                #         for root_dir, cur_dir, files in os.walk(r'.\predicted'):
+                #             count += len(files)
+                #         with open(os.path.join(path,file_up.name), "wb") as f:
+                #             f.write(file_up.getbuffer())
+                #         st.success(user['Name']+ " ชั้น " + user['Room']+ " เลขที่ " +user['Number']+" ส่ง "+labels[0][0]+" แล้ว")
+                    count = 0
+                    path = './predicted/'+ room + '/' + name + '/' + tissue
+                    isExist = os.path.exists(path)
+                    if not isExist:
+                            os.makedirs(path)
+                    for root_dir, cur_dir, files in os.walk(r'.\predicted'):
+                            count += len(files)
+                    with open(os.path.join(path,file_up.name), "wb") as f:
+                            f.write(file_up.getbuffer())
+                    cpt = sum([len(files) for r, d, files in os.walk(path)]) -1
+                    os.rename(f"{path}/{file_up.name}",f'{path}/{cpt}.jpg')
+                    st.success(user['Name']+ " ชั้น " + user['Room']+ " เลขที่ " +user['Number']+" ส่ง "+labels[0][0]+" แล้ว")
+                    path=os.path.join(room+'.csv')
+                    with open(path, 'a',encoding="utf-8") as csvfile:
+                        writer_object = writer(csvfile)
+                
+                    # Pass the list as an argument into
+                    # the writerow()
+                        writer_object.writerow([name,room,num,tissue])
+                        csvfile.close()
+                else:
+                    st.error("กรุณากรอกข้อมูลให้ครบถ้วน") 
+        # enable users to upload images for the model to make predictions
+    st.write("")
+with st.container():
+    cpt = sum([len(files) for r, d, files in os.walk('./predicted/')])
+    st.metric(label="Image Predicted", value=cpt, delta=1)
